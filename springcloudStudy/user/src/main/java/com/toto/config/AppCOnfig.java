a43deb754a4b28ffb87c73d4315bdc21816fa286
ynwrd <ynwrd@qq.com>
2019-10-11 21:47:02 +0800

    Ribbon 配置与测试

--- springcloudStudy/user/src/main/java/com/toto/config/AppCOnfig.java
+++ springcloudStudy/user/src/main/java/com/toto/config/AppCOnfig.java
@@ -1,6 +1,7 @@
 package com.toto.config;
 
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,6 +9,7 @@ import org.springframework.web.client.RestTemplate;
 public class AppCOnfig {
 
     @Bean
+    @LoadBalanced //加入这行就会自动使用Ribbon服务
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
     }
