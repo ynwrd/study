0a08adc929723246b75967a80c81f4d987771ea1
ynwrd <ynwrd@qq.com>
2019-10-11 17:34:45 +0800

    eureka集群配置

--- springcloudStudy/README.md
+++ springcloudStudy/README.md
@@ -1,5 +1,8 @@
 # springcloudStudy
 学习spring cloud的子项目：
-eureka 为服务端
+eureka 集群
+eureka 为服务端1,端口为3000
+eureka3001 为服务端2,端口为3001       
+eureka3002 为服务端2,端口为3002
 user 为分离的第一个客户端
 order 模仿订单信息，第二个客户端
---
+++ springcloudStudy/eureka3002/src/main/java/com/toto/Application3002.java
@@ -0,0 +1,13 @@
+package com.toto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class Application3002 {
+    public static void main(String[] args) {
+        SpringApplication.run(Application3002.class);
+    }
+}
